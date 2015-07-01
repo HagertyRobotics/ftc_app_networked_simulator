9599889d9e2dfce7a70e3b32061d477e4641093b
HagertyRobotics <crabmeat99@gmail.com>
2015-06-30 21:40:38 -0400

    Remove all of the simulation stuff so we can start with a master copy and branch the "on phone simulator" and the "off phone simulator".

--- FtcRobotController/src/main/java/com/ftdi.j2xx/FT_Device.java
+++ FtcRobotController/src/main/java/com/ftdi.j2xx/FT_Device.java
@@ -1,30 +1,14 @@
 package com.ftdi.j2xx;
 
-import android.os.SystemClock;
-import android.util.Log;
-
-import com.qualcomm.robotcore.util.TypeConversion;
-
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
 public class FT_Device
 {
     private static final String TAG = "FTDI_Device::";
     D2xxManager.FtDeviceInfoListNode mDeviceInfoNode;
     String mySerialNumber;
-    int mMotor1Encoder;  // Simulate motor 1 encoder.  Increment each write.
-    int mMotor2Encoder;  // Simulate motor 2 encoder.  Increment each write.
-    double mMotor1TotalError;
-    double mMotor2TotalError;
+
     long mTimeInMilliseconds=0;
     long mOldTimeInMilliseconds=0;
     long mDeltaWriteTime=0;
-
-    protected final byte[] mCurrentStateBuffer = new byte[208];
-
-    protected final Queue<CacheWriteRecord> readQueue = new ConcurrentLinkedQueue();
-    protected volatile boolean writeLocked = false;
 
     public FT_Device(String serialNumber, String description)
     {
@@ -34,10 +18,6 @@ public class FT_Device
         this.mDeviceInfoNode.serialNumber = serialNumber;
         this.mDeviceInfoNode.description = description;
 
-        mMotor1Encoder = 0;
-        mMotor2Encoder = 0;
-        mMotor1TotalError = 0;  // used in the RUN_TO_POSITION PID
-        mMotor2TotalError = 0;
     }
 
 
@@ -49,36 +29,6 @@ public class FT_Device
     public int read(byte[] data, int length, long wait_ms)
     {
         int rc = 0;
-        Object localObject1;
-        String logString[];
-
-        if (length <= 0) {
-            return -2;
-        }
-
-        try
-        {
-            this.writeLocked = true;
-
-            if (!this.readQueue.isEmpty()) {
-                localObject1 = this.readQueue.poll();
-                if (localObject1 == null)
-                    return rc;
-
-                System.arraycopy(((CacheWriteRecord)localObject1).data, 0, data, 0, length);
-                rc = length;
-//                if (length == 5) {
-//                    Log.v("Legacy", "READ: Response Header (" + bufferToHexString(data,0,length) + ") len=" + length);
-//                } else if (length == 3) {
-//                    Log.v("Legacy", "READ: Response Header (" + bufferToHexString(data,0,length) + ") len=" + length);
-//                } else if (length == 208) {
-//                    Log.v("Legacy", "READ: Response Buffer S0 (" + bufferToHexString(data,16+4,20) + "...) len=" + length);
-//                    Log.v("Legacy", "READ: Response Buffer FLAGS 0=" + bufferToHexString(data,0,3) + " 16=" + bufferToHexString(data,16,4) + "47=" + bufferToHexString(data,47,1));
-//                }
-            }
-        } finally {
-            this.writeLocked = false;
-        }
 
         return rc;
     }
@@ -100,16 +50,6 @@ public class FT_Device
         return write(data, length, true);
     }
 
-    public void queueUpForReadFromPhone(byte[] data) {
-        //while (this.writeLocked) Thread.yield();
-        this.readQueue.add(new CacheWriteRecord(data));
-    }
-    protected final byte[] writeCmd = { 85, -86, 0, 0, 0 };
-    protected final byte[] readCmd = { 85, -86, -128, 0, 0 };
-    protected final byte[] recSyncCmd3 = { 51, -52, 0, 0, 3};
-    protected final byte[] recSyncCmd0 = { 51, -52, -128, 0, 0};
-    protected final byte[] recSyncCmd208 = { 51, -52, -128, 0, (byte)208};
-    protected final byte[] controllerTypeLegacy = { 0, 77, 73};       // Controller type USBLegacyModule
 
     public int write(byte[] data, int length, boolean wait)
     {
@@ -119,174 +59,10 @@ public class FT_Device
             return rc;
         }
 
-        // Write Command
-        if (data[0] == writeCmd[0] && data[2] == writeCmd[2]) {  // writeCmd
 
-
-            // If size is 208(0xd0) bytes then they are writing a full buffer of data to all ports.
-            // Note: the buffer we were giving in this case is 208+5 bytes because the "writeCmd" header is attached
-            if (data[4] == (byte)0xd0 ) {
-                //Log.v("Legacy", "WRITE: Write Header (" + bufferToHexString(data,0,5) + ") len=" + length);
-                queueUpForReadFromPhone(recSyncCmd0); // Reply we got your writeCmd
-
-                //Log.v("Legacy", "WRITE: Write Buffer S0 (" + bufferToHexString(data, 5+16+4, 20) + ") len=" + length);
-                //Log.v("Legacy", "WRITE: Write Buffer FLAGS 0=" + bufferToHexString(data,5+0,3) + " 16=" + bufferToHexString(data,5+16,4) + "47=" + bufferToHexString(data,5+47,1));
-
-                // Now, the reset of the buffer minus the header 5 bytes should be 208 (0xd0) bytes that need to be written to the connected devices
-                // Write the entire received buffer into the mCurrentState buffer so the android can see what we are up to
-                // Note: the buffer we were giving in this case is 208+5 bytes because the "writeCmd" header is attached
-                System.arraycopy(data, 5, mCurrentStateBuffer, 0, 208);
-
-                // Check delta time to see if we are too slow in our simulation.
-                // Baud rate was 250,000 with real USB port connected to module
-                // We are getting deltas of 31ms between each write call
-                mTimeInMilliseconds = SystemClock.uptimeMillis();
-                mDeltaWriteTime = mTimeInMilliseconds - mOldTimeInMilliseconds;
-                mOldTimeInMilliseconds = mTimeInMilliseconds;
-                Log.v("Legacy", "WRITE: Delta Time = " + mDeltaWriteTime);
-
-                // This is for Port P0 only.  16 is the base offset.  Each port has 32 bytes.
-                // If I2C_ACTION is set, take some action
-                if (mCurrentStateBuffer[47] == (byte)0xff) { // Action flag
-                    if ((mCurrentStateBuffer[16] & (byte)0x01) == (byte)0x01) { // I2C Mode
-                        if ((mCurrentStateBuffer[16] & (byte)0x80) == (byte)0x80) { // Read mode
-                            // just for fun, simulate reading the encoder from i2c.
-                            // really just from the mMotor1Encoder class variable
-                            // 4 bytes of header (r/w, i2c address, i2c register, i2c buffer len)
-                            // +4 to get past the header, motor 1 encoder starts at 12 and is 4 bytes long
-                            // See Tetrix Dc Motor Controller data sheet
-                            mCurrentStateBuffer[16+4+12] = (byte)(mMotor1Encoder >> 24);
-                            mCurrentStateBuffer[16+4+12+1] = (byte)(mMotor1Encoder >> 16);
-                            mCurrentStateBuffer[16+4+12+2] = (byte)(mMotor1Encoder >> 8);
-                            mCurrentStateBuffer[16+4+12+3] = (byte)(mMotor1Encoder);
-
-                            mCurrentStateBuffer[16+4+16] = (byte)(mMotor2Encoder >> 24);
-                            mCurrentStateBuffer[16+4+16+1] = (byte)(mMotor2Encoder >> 16);
-                            mCurrentStateBuffer[16+4+16+2] = (byte)(mMotor2Encoder >> 8);
-                            mCurrentStateBuffer[16+4+16+3] = (byte)(mMotor2Encoder);
-
-                        } else { // Write mode
-
-                            // Just for fun, simulate some of the motor modes
-                            simulateWritesToMotor1();
-                            simulateWritesToMotor2();
-                        }
-                    }
-                }
-
-                // Set the Port S0 ready bit in the global part of the Current State Buffer
-                mCurrentStateBuffer[3] = (byte)0xfe;  // Port S0 ready
-
-            }
-        // Read Command
-        } else if (data[0] == readCmd[0] && data[2] == readCmd[2]) { // readCmd
-            if (data[4] == 3) { // Android asks for 3 bytes, initial query of device type
-                //Log.v("Legacy", "WRITE: Read Header (" + bufferToHexString(data,0,length) + ") len=" + length);
-                queueUpForReadFromPhone(recSyncCmd3);  // Send receive sync, bytes to follow
-                queueUpForReadFromPhone(controllerTypeLegacy);
-            } else if (data[4] == (byte)208) { // Android asks for 208 bytes, full read of device
-                //Log.v("Legacy", "WRITE: Read Header (" + bufferToHexString(data,0,length) + ") len=" + length);
-                queueUpForReadFromPhone(recSyncCmd208);  // Send receive sync, bytes to follow
-                queueUpForReadFromPhone(mCurrentStateBuffer); //
-            }
-        }
-
-        rc = length;
         return rc;
     }
-
-    private void simulateWritesToMotor1() {
-        // Simulate writes to Motor 1
-        // Check for the 4 different states
-        switch ((mCurrentStateBuffer[16+4+4] & (byte)0x03)) {
-            case (byte)0x00:  // Run with no encoder
-                // Check the motor power we just received and increment/decrement encoder
-                // For now just increment encoder by motor power
-                // Note: Float power = 0x80 (Negative Zero)
-                if ((mCurrentStateBuffer[16+4+5] != (byte)0x00) && (mCurrentStateBuffer[16+4+5] != (byte)0x80)) {  // Motor 1 Power
-                        mMotor1Encoder += mCurrentStateBuffer[16+4+5];
-                }
-                break;
-            case (byte)0x01:  // Run with PID on encoder
-                break;
-            case (byte)0x02:  // Run to position
-                double P = 0.5;
-                double I = 0.05;
-                int error = getMotor1TargetEncoder() - getMotor1CurrentEncoder();
-                mMotor1TotalError += error;
-                if (mMotor1TotalError > 2000) mMotor1TotalError = 2000;
-                if (mMotor1TotalError < -2000) mMotor1TotalError = -2000;
-
-                //Log.v("Legacy", "PID: " + error + " " + mMotor1TotalError);
-                int power = (int)(P*error) + (int)(mMotor1TotalError*I);
-                if (power > 100) power=100;
-                if (power < -100) power= -100;
-                mMotor1Encoder += power;
-                break;
-            case (byte)0x03:  // Reset encoder
-                mMotor1Encoder = 0;
-                break;
-        }
-    }
-
-    private void simulateWritesToMotor2() {
-        // Simulate writes to Motor 2
-        // Check for the 4 different states
-        switch ((mCurrentStateBuffer[16+4+7] & (byte)0x03)) {
-            case (byte)0x00:  // Run with no encoder
-                // Check the motor power we just received and increment/decrement encoder
-                // For now just increment encoder by motor power
-                // Note: Float power = 0x80 (Negative Zero)
-                if ((mCurrentStateBuffer[16+4+6] != (byte)0x00) && (mCurrentStateBuffer[16+4+6] != (byte)0x80)) {  // Motor 2 Power
-                    mMotor1Encoder += mCurrentStateBuffer[16+4+6];
-                }
-                break;
-            case (byte)0x01:  // Run with PID on encoder
-                break;
-            case (byte)0x02:  // Run to position
-                double P = 0.5;
-                double I = 0.05;
-                int error = getMotor2TargetEncoder() - getMotor2CurrentEncoder();
-                mMotor2TotalError += error;
-                if (mMotor2TotalError > 2000) mMotor2TotalError = 2000;
-                if (mMotor2TotalError < -2000) mMotor2TotalError = -2000;
-
-                //Log.v("Legacy", "PID: " + error + " " + mMotor2TotalError);
-                int power = (int)(P*error) + (int)(mMotor2TotalError*I);
-                if (power > 100) power=100;
-                if (power < -100) power= -100;
-                mMotor2Encoder += power;
-                break;
-            case (byte)0x03:  // Reset encoder
-                mMotor2Encoder = 0;
-                break;
-        }
-    }
-
-    private int getMotor1CurrentEncoder() {
-        byte[] arrayOfByte1 = new byte[4];
-        System.arraycopy(mCurrentStateBuffer, 16+4+12, arrayOfByte1, 0, arrayOfByte1.length);
-        return TypeConversion.byteArrayToInt(arrayOfByte1);
-    }
-
-    private int getMotor2CurrentEncoder() {
-        byte[] arrayOfByte1 = new byte[4];
-        System.arraycopy(mCurrentStateBuffer, 16+4+16, arrayOfByte1, 0, arrayOfByte1.length);
-        return TypeConversion.byteArrayToInt(arrayOfByte1);
-    }
-
-    private int getMotor1TargetEncoder() {
-        byte[] arrayOfByte1 = new byte[4];
-        System.arraycopy(mCurrentStateBuffer, 16+4, arrayOfByte1, 0, arrayOfByte1.length);
-        return TypeConversion.byteArrayToInt(arrayOfByte1);
-    }
-
-    private int getMotor2TargetEncoder() {
-        byte[] arrayOfByte1 = new byte[4];
-        System.arraycopy(mCurrentStateBuffer, 16+4+8, arrayOfByte1, 0, arrayOfByte1.length);
-        return TypeConversion.byteArrayToInt(arrayOfByte1);
-    }
-
+    
     private String bufferToHexString(byte[] data, int start, int length) {
         int i;
         int myStop;
