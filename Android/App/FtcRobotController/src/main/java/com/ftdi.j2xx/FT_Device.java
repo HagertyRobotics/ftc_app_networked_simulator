1262a09f77478fbe24dd73ca0026df327404884d
HagertyRobotics <crabmeat99@gmail.com>
2015-07-06 11:05:52 -0400

    Added some code to enable timeouts on the socket reads.  When there is not a PC connection the phone will get stuck reading.

--- Android/App/FtcRobotController/src/main/java/com/ftdi.j2xx/FT_Device.java
+++ Android/App/FtcRobotController/src/main/java/com/ftdi.j2xx/FT_Device.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -38,6 +39,7 @@ public class FT_Device
             mSimulatorSocket = new Socket("10.0.1.193", SERVERPORT);
             os = new DataOutputStream(mSimulatorSocket.getOutputStream());
             is = new DataInputStream(mSimulatorSocket.getInputStream());
+            mSimulatorSocket.setSoTimeout(200);
         } catch (UnknownHostException e) {
             Log.v("D2xx::", "Error: " + e);
         } catch (IOException e) {
@@ -46,7 +48,9 @@ public class FT_Device
     }
 
 
-
+    /*
+    *
+    */
     public void sendBytesToSimulator(byte[] data) {
         // If everything has been initialized then we want to write some data
         // to the socket we have opened a connection to on port 25
@@ -61,26 +65,57 @@ public class FT_Device
         }
     }
 
-    private void readBytesFromSimulator(byte[] data, int length) {
-        int totalRead = 0;
-
-        while (totalRead < length) {
-            int bytesRead = 0;
-            try {
-                bytesRead = is.read(data, totalRead, length - totalRead);
-            } catch (IOException e) {
-                Log.v("D2xx::", "Error: " + e);
-                e.printStackTrace();
-            }
-
-            totalRead += bytesRead;
+    /*
+    *
+    */
+    private int readBytesFromSimulator(byte[] data, int length) {
+        int bytesRead = 0;
+        Log.v("D2xx::", "Read: Length: " + length);
+        try {
+            bytesRead = is.read(data, 0, length);
+        } catch (IOException e) {
+            Log.v("D2xx::", "Error: " + e);
+            e.printStackTrace();
         }
+
+        try {
+            if (bytesRead != length) {
+                int skipped = is.skipBytes(length);
+                Log.v("D2xx::", "Skipped: " + skipped);
+            }
+        } catch (IOException e) {
+            Log.v("D2xx::", "Error: " + e);
+            e.printStackTrace();
+        }
+
+        Log.v("D2xx::", "Read: BytesRead: " + bytesRead);
+        return bytesRead;
     }
 
-    public synchronized void close()
-    {
 
+    // readInputStreamWithTimeout
+    //
+    // Considered using this to implement a timeout.  Tried setting timeout in socket instead.
+    //
+    // example:
+    // byte[] inputData = new byte[1024];
+    // int readCount = readInputStreamWithTimeout(System.in, inputData, 6000);  // 6 second timeout
+    // readCount will indicate number of bytes read; -1 for EOF with no data read.
+    //
+    public static int readInputStreamWithTimeout(InputStream is, byte[] b, int timeoutMillis)
+            throws IOException  {
+        int bufferOffset = 0;
+        long maxTimeMillis = System.currentTimeMillis() + timeoutMillis;
+        while (System.currentTimeMillis() < maxTimeMillis && bufferOffset < b.length) {
+            int readLength = java.lang.Math.min(is.available(),b.length-bufferOffset);
+            // can alternatively use bufferedReader, guarded by isReady():
+            int readResult = is.read(b, bufferOffset, readLength);
+            if (readResult == -1) break;
+            bufferOffset += readResult;
+        }
+        return bufferOffset;
     }
+
 
     public int read(byte[] data, int length, long wait_ms)
     {
@@ -90,34 +125,19 @@ public class FT_Device
             return -2;
         }
         readBytesFromSimulator(data, length);
-        Log.v("D2xx::", "Read: " + bufferToHexString(data, 0, data.length));
+        Log.v("D2xx::", "Read: (" + wait_ms + ") " + bufferToHexString(data, 0, data.length));
 
         rc = length;
         return rc;
     }
 
-    public int read(byte[] data, int length)
-    {
-        long l=0;
-        return read(data, length, l);
-    }
 
-    public int read(byte[] data)
-    {
-        long l=0;
-        return read(data, data.length,l);
-    }
-
-    public int write(byte[] data, int length)
-    {
-        return write(data, length, true);
-    }
 
 
     public int write(byte[] data, int length, boolean wait)
     {
         int rc = 0;
-        Log.v("D2xx::", "Write: " + bufferToHexString(data,0,data.length));
+        Log.v("D2xx::", "Write: (" + wait + ") " + bufferToHexString(data,0,data.length));
         if (length <= 0) {
             return rc;
         }
@@ -137,6 +157,27 @@ public class FT_Device
             sb.append(String.format("%02x ", data[i]));
         }
         return sb.toString();
+    }
+
+
+    //////////////////////////////////  Stubs from original FTDI Class  /////////////////////////////////////
+
+
+    public int read(byte[] data, int length)
+    {
+        long l=0;
+        return read(data, length, l);
+    }
+
+    public int read(byte[] data)
+    {
+        long l=0;
+        return read(data, data.length,l);
+    }
+
+    public int write(byte[] data, int length)
+    {
+        return write(data, length, true);
     }
 
     public int write(byte[] data)
@@ -170,5 +211,10 @@ public class FT_Device
         public CacheWriteRecord(byte[] data) {
             this.data = data;
         }
+    }
+
+    public synchronized void close()
+    {
+
     }
 }
