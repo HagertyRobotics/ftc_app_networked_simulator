4fe26bb3f446f583d95d85aeb8f49d922c0fe381
HagertyRobotics <crabmeat99@gmail.com>
2015-07-06 18:24:00 -0400

    Added a NetworkManager class that has two inner threads to receive and transmit packets to the PC Simulator

---
+++ Android/App/FtcRobotController/src/main/java/com/ftdi.j2xx/NetworkManager.java
@@ -0,0 +1,141 @@
+package com.ftdi.j2xx;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+/**
+ *
+ */
+public class NetworkManager {
+
+    // the Server's Port
+    public static final int PHONEPORT  = 6000;
+
+    DatagramSocket mSimulatorSocket;
+    LinkedBlockingQueue mWriteToPcQueue = new LinkedBlockingQueue();
+    LinkedBlockingQueue mReadFromPcQueue = new LinkedBlockingQueue();
+
+
+    public NetworkManager() {
+        // Start the Network Sender thread
+        // This thread will read from the mWriteToPcQueue and send packets to the PC application
+        // The mWriteToPcQueue will get packets from the FT_Device write call
+        try {
+            mSimulatorSocket = new DatagramSocket(PHONEPORT);
+            Log.v("D2xx::", "Local Port " + mSimulatorSocket.getLocalPort());
+            NetworkSender myNetworkSender = new NetworkSender(mWriteToPcQueue, mSimulatorSocket);  // Runnable
+            Thread networkSenderThread = new Thread(myNetworkSender);
+            networkSenderThread.start();
+
+            NetworkReceiver myNetworkReceiver = new NetworkReceiver(mReadFromPcQueue, mSimulatorSocket);
+            Thread networkReceiverThread = new Thread(myNetworkReceiver);
+            networkReceiverThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    LinkedBlockingQueue getWriteToPcQueue() {
+        return mWriteToPcQueue;
+    }
+
+    LinkedBlockingQueue getReadFromPcQueue() {
+        return mReadFromPcQueue;
+    }
+
+
+
+    /**
+     * NetworkRecevier
+     * Receive packets from the PC simulator and feed them into a queue that the FT_Device class will read
+     * The FT_Device class will pretend to be a FTDI USB device and feed the
+     * received packets to the FTC_APP
+     *
+     */
+    public class NetworkReceiver implements Runnable {
+        private LinkedBlockingQueue queue;
+        DatagramSocket mSocket;
+        byte[] mReceiveData = new byte[1024];
+
+        public NetworkReceiver(LinkedBlockingQueue queue, DatagramSocket my_socket) {
+            this.queue = queue;
+            this.mSocket = my_socket;
+        }
+
+
+        @Override
+        public void run() {
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(mReceiveData, mReceiveData.length);
+                try {
+                    mSocket.receive(receivePacket);
+
+                    byte[] readBuffer = new byte[receivePacket.getLength()];
+                    System.arraycopy(receivePacket.getData(), 0, readBuffer, 0, receivePacket.getLength());
+                    Log.v("D2xx::", "Receive: " + readBuffer[0] + "Len: " + receivePacket.getLength());
+                    queue.put(readBuffer);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    /**
+     * NetworkSender
+     * Send packets to the PC simulator.
+     * Pull the packets from the passed queue.  Packets were queued in the FT_Device class after begin
+     * received from the USB transmit functions of this app.  The FT_Device class is simulating the
+     * USB stack using a UDP network connection to the PC simulator.
+     *
+     */
+    public class NetworkSender implements Runnable {
+        private LinkedBlockingQueue queue;
+
+        DatagramSocket mSocket;
+        InetAddress IPAddress;
+
+        public NetworkSender(LinkedBlockingQueue queue, DatagramSocket mySocket) {
+            this.queue = queue;
+            this.mSocket = mySocket;
+
+            try {
+                IPAddress = InetAddress.getByName("10.0.1.193");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void run() {
+            byte[] writeBuf;
+
+            while (true) {
+                try {
+                    writeBuf = (byte[])queue.take();
+                    DatagramPacket send_packet = new DatagramPacket(writeBuf,writeBuf.length, IPAddress, 6500);
+                    mSocket.send(send_packet);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+}
