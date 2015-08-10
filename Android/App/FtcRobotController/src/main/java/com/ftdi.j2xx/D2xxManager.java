992b781a17c5141384c6722f08c888295d6b7c65
David S <dmssargent@yahoo.com>
2015-08-10 11:19:30 -0500

    Sync with upstream

--- Android/App/FtcRobotController/src/main/java/com/ftdi.j2xx/D2xxManager.java
+++ Android/App/FtcRobotController/src/main/java/com/ftdi.j2xx/D2xxManager.java
@@ -21,7 +21,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
 
